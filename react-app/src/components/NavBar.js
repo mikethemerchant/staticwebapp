fbd3542a8a4d68ebe4f2e21e9a50b78439c7765f
Michael Bender <michael.bender@woodgrain.com>
2025-11-10 11:54:51 -0500

    Refactor NavBar and Products components to use updated routing methods with className for active links

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -5,10 +5,10 @@ const NavBar = (props) => (
   <nav className="column is-2 menu">
     <p className="menu-label">Menu</p>
     <ul className="menu-list">
-      <NavLink to="/products" activeClassName="active-link">
+      <NavLink to="/products" className={({ isActive }) => isActive ? "active-link" : ""}>
         Products
       </NavLink>
-      <NavLink to="/about" activeClassName="active-link">
+      <NavLink to="/about" className={({ isActive }) => isActive ? "active-link" : ""}>
         About
       </NavLink>
     </ul>
